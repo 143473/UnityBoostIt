2eed86147fecd9eaebab3e56a07dde065b905db0
143473 <75113646+143473@users.noreply.github.com>
2022-03-18 20:19:14 +0100

    Added Assets prefabs, materials, movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] protected float mainThrust = 1000f;
+    [SerializeField] protected float rotateThrust = 1f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateTransform(rotateThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateTransform(-rotateThrust);
+        }
+    }
+
+    void RotateTransform(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+    }
+}
